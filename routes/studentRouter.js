cc3277461ff28a8730401b6f93d2ec1d73345d01
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-02-20 22:10:04 +0530

    write req and res

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,3 +1,12 @@
 import express from 'express';
+import Student from '../model/students';
 
 const studentRouter = express.Router();
+
+StudentRouter.get('/', async (req, res) => {
+    console.log('Received GET request for all students');
+
+    res.json({ message: 'GET request received for all students' });
+});
+
+export default studentRouter;
