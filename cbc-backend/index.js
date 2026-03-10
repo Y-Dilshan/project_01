ba8f73b6c493f33dda7e855c90109c42333aea89
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-03-10 23:43:09 +0530

    connected mongo db

--- cbc-backend/index.js
+++ cbc-backend/index.js
@@ -3,4 +3,13 @@ import bodyParser from 'body-parser';
 import Student from '../model/students.js';
 
 const app = express();
-app.use(bodyParser.json());
+
+const mongoUrl = "mongodb+srv://admin:123@cluster0.v1bzlcn.mongodb.net/?appName=Cluster0"
+
+app.get("/", (req, res) => {
+    res.send('Hello World');
+});
+
+app.post("/", ()=>{
+    console.log("Post request received");
+});
