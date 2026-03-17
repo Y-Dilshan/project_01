ea4aea78f59928d3114b9d0e61acdd144dfcc05c
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-03-17 13:45:10 +0530

    added nodemon

--- cbc-backend/index.js
+++ cbc-backend/index.js
@@ -1,15 +1,34 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import Student from '../model/students.js';
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://admin:123@cluster0.v1bzlcn.mongodb.net/?appName=Cluster0"
+// Middleware
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send('Hello World');
+// MongoDB Connection
+const mongoUrl = "mongodb+srv://admin:123@cluster0.v1bzlcn.mongodb.net/testDB?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUrl)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log("DB Error:", err);
 });
 
-app.post("/", ()=>{
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
     console.log("Post request received");
+    res.send("POST request successful");
+});
+
+// Server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
