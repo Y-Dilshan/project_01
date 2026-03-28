6024c0e2afb2671f2f251b3406975ea3e8f4f772
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-03-28 16:22:19 +0530

    added student methods into studentRouter

--- cbc-backend/index.js
+++ cbc-backend/index.js
@@ -15,51 +15,6 @@ const app = express();
 // Middleware (IMPORTANT for JSON body parsing)
 app.use(express.json());
 
-// GET → use query params
-app.get("/", (req, res) => {
-    Student.find().then(
-    (student)=>{
-        res.json(student);
-    }
-    )
-});
-
-// POST → use JSON body
-app.post("/", (req, res) => {
-    console.log(req.body);
-
-    const student = new Student(req.body);
-    student.save().then(
-        ()=>{
-            res.json({
-                message : "Student saved successfully"
-            })
-        }
-    )
-});
-
-// DELETE → use JSON body
-app.delete("/", (req, res) => {
-    const name = req.body.name;
-
-    console.log("DELETE name:", name);
-
-    res.json({
-        message: name ? "Deleted " + name : "Name not provided"
-    });
-});
-
-// PUT → use JSON body
-app.put("/", (req, res) => {
-    const name = req.body.name;
-
-    console.log("PUT name:", name);
-
-    res.json({
-        message: name ? "Updated " + name : "Name not provided"
-    });
-});
-
 // Start server
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
---
+++ cbc-backend/routes/studentRouter.js
@@ -0,0 +1,35 @@
+import express from "express";
+import Student from "../models/student.js";
+
+const studentRouter = express.Router();
+
+studentRouter.get("/", (req, res) => {
+    Student.find().then(
+    (student)=>{
+        res.json(student);
+    }
+    )
+});
+
+studentRouter.post("/", (req, res) => {
+    const student = new Student(req.body);
+    student.save().then(
+        ()=>{
+            res.json({
+                message : "Student saved successfully"
+            })
+        }
+    )
+});
+
+studentRouter.delete("/", (req, res) => {
+    res.json({
+        message: "DELETE request received"
+    });
+});
+
+studentRouter.put("/", (req, res) => {
+    res.json({
+        message: "PUT request received" + req.body.name
+    });
+});
