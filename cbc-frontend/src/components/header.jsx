8ef587198589e746504eb8bc697733b6bbdc9384
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-04-11 22:13:13 +0530

    started to learn react

--- cbc-frontend/src/components/header.jsx
+++ cbc-frontend/src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-            <h1>My Travel Journal</h1>
+            <h1 className="bg-amber-800 text-center">My Travel Journal</h1>
         </header>
     )
 }
