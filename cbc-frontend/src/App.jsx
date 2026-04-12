d3950d6379892a0c97b177b57578672d990175d4
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-04-12 12:11:02 +0530

    started to learn usestate

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -3,14 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Header from './components/header'
+import Test from './components/test.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header />
+      <Test />
     </>
   )
 }
--- cbc-frontend/src/components/header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return(
-        <header>
-            <h1 className="bg-amber-800 text-center">My Travel Journal</h1>
-        </header>
-    )
-}
