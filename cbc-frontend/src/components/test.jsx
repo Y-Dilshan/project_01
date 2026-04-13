69a47da8645dd5427bab4ba730a5c0b15c4dcb5f
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-04-13 20:44:36 +0530

    running function using usestate

--- cbc-frontend/src/components/test.jsx
+++ cbc-frontend/src/components/test.jsx
@@ -1,18 +1,20 @@
+import {useState} from "react";
+
 export default function Test() {
 
-    let count = 0;
+    const [count , setCount] = useState(0)
 
     return (
         <div className="w-full h-screen flex items-center justify-center">
             <div className="w-[400px] h-[300px] shadow-2xl flex items-center justify-center">
                 <button className="w-[100px] h-[50px] bg-blue-500 text-white" onClick = {()=>{
-                    count = count -1
+                    setCount(count - 1)
                 }}>Decrement</button>
 
                 <span className="w-[100px] h-[50px] text-2xl font-bold text-center">{count}</span>
 
                 <button className="w-[100px] h-[50px] bg-green-500 text-white" onClick ={()=>{
-                    count = count +1
+                    setCount(count + 1)
                 }}>Increment</button>
             </div>
         </div>
