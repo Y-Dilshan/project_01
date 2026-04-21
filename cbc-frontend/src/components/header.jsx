109c5b29e5ad8979fa51b561df041a9cb3096aac
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-04-21 22:17:55 +0530

    created header file

--- cbc-frontend/src/components/header.jsx
+++ cbc-frontend/src/components/header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className = "w-full h-[100px] bg-[#1E3A8A]">
+        <header className = "w-full h-[100px] bg-accent">
             <img src="logo.png" className="w-[80px] h-[80px]" />
-            <h1>My Shop</h1>
+            <h1 className="text-center text-justify-center">My Shop</h1>
         </header>
     );
 }
